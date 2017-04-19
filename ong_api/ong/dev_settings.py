0366e37e9f2f70b3d906eb6e17f08fe9712588c0
Alvaro Leonel <alvaro.lqueiroz@gmail.com>
2017-04-18 22:09:22 -0400

    Configurado ambiente de produção

--- ong_api/ong/dev_settings.py
+++ ong_api/ong/dev_settings.py
@@ -10,7 +10,11 @@ ALLOWED_HOSTS = ['*']
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'ong-control',
+        'USER': 'ong-api-user',
+        'PASSWORD': 'ong-123',
+        'HOST': '10.0.75.1',
+        'PORT': '5432',
     }
 }
--- ong_api/start_dev_srv.sh
+++
@@ -1,2 +0,0 @@
-/etc/init.d/nginx start &
-/usr/local/bin/uwsgi --ini api_uwsgi.ini --uid www-data --gid www-data
