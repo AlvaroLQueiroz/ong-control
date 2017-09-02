c78f18f6042d9e38d57147560bd4df841dfd0272
Alvaro Leonel <alvaro.lqueiroz@gmail.com>
2017-09-01 20:56:02 -0400

    Fix apache config

--- ong_api/deploy.sh
+++ ong_api/deploy.sh
@@ -1,8 +1,8 @@
 git pull
-python manage.py migrate
+python manage.py migrate --noinput
 python manage.py loaddata fixtures/auth_groups.json
 python manage.py loaddata fixtures/users.json
 python manage.py loaddata fixtures/phone_operators.json
-python manage.py collectstatic
+python manage.py collectstatic --noinput
 cp ../apache/ong-control.conf /etc/apache2/sites-available/ong-control.conf
 service apache2 restart
