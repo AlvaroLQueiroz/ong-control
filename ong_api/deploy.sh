d8285d2035c78b9673bf6e6719064535946b8943
Alvaro Leonel <alvaro.lqueiroz@gmail.com>
2017-09-01 20:04:11 -0400

    New deploy

--- ong_api/deploy.sh
+++ ong_api/deploy.sh
@@ -4,3 +4,5 @@ python manage.py loaddata fixtures/auth_groups.json
 python manage.py loaddata fixtures/users.json
 python manage.py loaddata fixtures/phone_operators.json
 python manage.py collectstatic
+cp ../apache/ong-control.conf /etc/apache2/sites-available/ong-control.conf
+service apache2 restart
