84c927b1f01fd6e31de6641dd803b56ea2486497
Alvaro Leonel <alvaro.lqueiroz@gmail.com>
2017-09-05 14:52:55 -0400

    Fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -9,9 +9,9 @@ service nginx restart
 mkdir /var/log/uwsgi
 
 cd ong_api
-python manage.py migrate --noinput
-python manage.py loaddata fixtures/auth_groups.json
-python manage.py loaddata fixtures/users.json
-python manage.py loaddata fixtures/phone_operators.json
-python manage.py collectstatic --noinput
+python3 manage.py migrate --noinput
+python3 manage.py loaddata fixtures/auth_groups.json
+python3 manage.py loaddata fixtures/users.json
+python3 manage.py loaddata fixtures/phone_operators.json
+python3 manage.py collectstatic --noinput
 cd ..
