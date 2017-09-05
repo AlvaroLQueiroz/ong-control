f0090d9990f1ce315dd29dcf35ba54d0f0a1c292
Alvaro Leonel <alvaro.lqueiroz@gmail.com>
2017-09-05 10:01:36 -0400

    Migrating to D.O.

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,12 @@
 git pull
+pip3 install -r ong_api/requirements.prod.txt
 ln -s ./ong.nginx.conf /etc/nginx/sites-enabled/
-chmod 644 /etc/nginx/sites-enabled/ong.nginx.conf
 cd ong_api
 python manage.py migrate --noinput
 python manage.py loaddata fixtures/auth_groups.json
 python manage.py loaddata fixtures/users.json
 python manage.py loaddata fixtures/phone_operators.json
 python manage.py collectstatic --noinput
-python manage.py runserver
+cd ..
+servie nginx reload
+servie nginx restart
