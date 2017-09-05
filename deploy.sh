2ff52659cde567fcc4bea14707ebbb3f2270998e
Alvaro Leonel <alvaro.lqueiroz@gmail.com>
2017-09-05 14:35:50 -0400

    new deploy again

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,13 @@
 git pull
 pip3 install -r ong_api/requirements.prod.txt
-ln -s ./ong.nginx.conf /etc/nginx/sites-enabled/
+
+cp ./ong.nginx.conf /etc/nginx/sites-available
+ln -s /etc/nginx/sites-available /etc/nginx/sites-enabled/ong.conf
+service nginx reload
+service nginx restart
+
+mkdir /var/log/uwsgi
+
 cd ong_api
 python manage.py migrate --noinput
 python manage.py loaddata fixtures/auth_groups.json
@@ -8,5 +15,3 @@ python manage.py loaddata fixtures/users.json
 python manage.py loaddata fixtures/phone_operators.json
 python manage.py collectstatic --noinput
 cd ..
-servie nginx reload
-servie nginx restart
--- ong-webapp/src/environments/environment.prod.ts
+++ ong-webapp/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiAddress: '//143.106.60.113',
+  apiAddress: '//104.131.100.116',
   apiPort: '80',
   postfix: '/api',
 };
