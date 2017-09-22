be86456af246a6e24a756400967c0c3c1e4f979a
Alvaro Leonel <alvaro.lqueiroz@gmail.com>
2017-09-21 22:08:36 -0400

    fix django urls

--- ong_api/ong/urls.py
+++ ong_api/ong/urls.py
@@ -4,8 +4,8 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^api/admin/', admin.site.urls),
-    url(r'^api', include('finance.urls', namespace='finance')),
-    url(r'^api', include('collaborator.urls', namespace='collaborator')),
+    url(r'^api/', include('finance.urls', namespace='finance')),
+    url(r'^api/', include('collaborator.urls', namespace='collaborator')),
     url(r'^api/', include('user_profile.urls', namespace='profile')),
     url(r'^', include('core.urls', namespace='core')),
 ]
